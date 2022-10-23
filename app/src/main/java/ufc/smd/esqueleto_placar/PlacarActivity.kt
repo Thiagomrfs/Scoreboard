7065013ec0d7364c7e620dee78a35ed6e8aa1085
igorNeves007 <igorneves0404@gmail.com>
2022-10-23 17:09:24 -0300

    Individual points

--- app/src/main/java/ufc/smd/esqueleto_placar/PlacarActivity.kt
+++ app/src/main/java/ufc/smd/esqueleto_placar/PlacarActivity.kt
@@ -1,6 +1,5 @@
 package ufc.smd.esqueleto_placar
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
@@ -23,29 +22,104 @@ import java.nio.charset.StandardCharsets
 
 class PlacarActivity : AppCompatActivity() {
     lateinit var placar:Placar
-    lateinit var tvResultadoJogo: TextView
-    var game =0
+
+
+    var timeEsquerda : Int = 0
+
+    var timeDireita : Int = 0
+
+    //Array dos pontos
+    var placarPontos = arrayListOf(
+        arrayListOf(0,0)
+    )
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_placar)
         placar= getIntent().getExtras()?.getSerializable("placar") as Placar
-        tvResultadoJogo= findViewById(R.id.tvPlacar)
+
         //Mudar o nome da partida
         val tvNomePartida=findViewById(R.id.tvNomePartida2) as TextView
         tvNomePartida.text=placar.nome_partida
         ultimoJogos()
     }
 
-    fun alteraPlacar (v:View){
-        game++
-        if ((game % 2) != 0) {
-            placar.resultado = ""+game+" vs "+ (game-1)
-        }else{
-            placar.resultado = ""+(game-1)+" vs "+ (game-1)
-            vibrar(v)
-        }
-        tvResultadoJogo.text=placar.resultado
+        //Retorna os pontos
+    fun getPontos() : ArrayList<ArrayList<Int>> {
+        return placarPontos
     }
+
+    //Adiciona aos pontos
+    fun addPontos(ponto : ArrayList<Int>){
+        placarPontos.add(ponto)
+    }
+
+    fun alteraPlacarEsquerda(v:View){
+        timeEsquerda += 1
+        var ultimoPonto : ArrayList<Int> = getPontos().last()
+
+        ultimoPonto[0] += 1
+
+        //Adiciona pontuação
+        addPontos(ultimoPonto)
+
+
+        Log.v("PLACAR", ultimoPonto.toString())
+        Log.v("PLACAR", placarPontos.toString())
+
+        //Atualiza placar
+        atualizaPlacar(v)
+    }
+
+    fun alteraPlacarDireita(v:View){
+        timeDireita += 1
+
+        var ultimoPonto : ArrayList<Int> = getPontos().last()
+
+        ultimoPonto[1] += 1
+
+        addPontos(ultimoPonto)
+
+
+
+        Log.v("PLACAR", ultimoPonto.toString())
+        Log.v("PLACAR", placarPontos.toString())
+
+
+        atualizaPlacar(v)
+    }
+
+    fun voltaPlacar(v:View){
+        placarPontos.removeLast()
+        atualizaPlacar(v)
+    }
+
+    fun atualizaPlacar(v:View){
+        val teamLeft = findViewById<TextView>(R.id.teamLeft)
+//        teamLeft.setText(timeEsquerda.toString())
+        teamLeft.setText(placarPontos.last()[0].toString())
+
+        val teamRight = findViewById<TextView>(R.id.teamRight)
+//        teamRight.setText(timeDireita.toString())
+        teamRight.setText(placarPontos.last()[1].toString())
+
+        placar.resultado = ""+timeEsquerda+" vs "+ timeDireita
+        vibrar(v)
+    }
+
+
+
+//    fun alteraPlacar (v:View){
+//        game++
+//        if ((game % 2) != 0) {
+//            placar.resultado = ""+game+" vs "+ (game-1)
+//        }else{
+//            placar.resultado = ""+(game-1)+" vs "+ (game-1)
+//            vibrar(v)
+//        }
+//        tvResultadoJogo.text=placar.resultado
+//    }
 
 
     fun vibrar (v:View){
@@ -81,6 +155,9 @@ class PlacarActivity : AppCompatActivity() {
         edShared.putString("match"+numMatches, dt.toString(StandardCharsets.ISO_8859_1.name()))
         edShared.commit() //Não esqueçam de comitar!!!
 
+        //Retorna para home
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 
     fun lerUltimosJogos(v: View){
